3abcd36de31403ef2c713a7f549f5c46ad9b198d
Nate Geslin <teamtomkins23@gmail.com>
2016-04-10 15:01:27 -0500

    Updates project_type routes to projectType

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   resources :task_items
-  resources :project_types
+  resources :project_types, :path => 'projectTypes'
   resources :projects
   resources :clients
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
--- spec/routing/project_types_routing_spec.rb
+++ spec/routing/project_types_routing_spec.rb
@@ -3,27 +3,27 @@ require "rails_helper"
 RSpec.describe ProjectTypesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/project_types").to route_to("project_types#index")
+      expect(:get => "/projectTypes").to route_to("project_types#index")
     end
 
     it "routes to #show" do
-      expect(:get => "/project_types/1").to route_to("project_types#show", :id => "1")
+      expect(:get => "/projectTypes/1").to route_to("project_types#show", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/project_types").to route_to("project_types#create")
+      expect(:post => "/projectTypes").to route_to("project_types#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/project_types/1").to route_to("project_types#update", :id => "1")
+      expect(:put => "/projectTypes/1").to route_to("project_types#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/project_types/1").to route_to("project_types#update", :id => "1")
+      expect(:patch => "/projectTypes/1").to route_to("project_types#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/project_types/1").to route_to("project_types#destroy", :id => "1")
+      expect(:delete => "/projectTypes/1").to route_to("project_types#destroy", :id => "1")
     end
   end
 end
