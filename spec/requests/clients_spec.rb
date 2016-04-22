b98aaac754163180fa7cabf3de3f923ab916d6e3
Nate Geslin <teamtomkins23@gmail.com>
2016-04-21 22:19:21 -0500

    Adds without authentication contexts to clients spec

--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -5,97 +5,190 @@ describe 'Clients', :type => :request do
   let(:user) { create(:user) }
 
   describe 'GET /clients' do
-    it 'returns all clients' do
+    before :each do
       FactoryGirl.create :client, name: 'John Doe'
+    end
 
-      get '/clients',
-          headers: {
-            'Content-Type' => 'application/json',
-            'Authorization' => user.access_token
-          }
+    context 'with authorization' do
+      it 'returns all clients' do
+        get '/clients',
+            headers: {
+              'Content-Type' => 'application/json',
+              'Authorization' => user.access_token
+            }
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        get '/clients',
+            headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
     end
   end
 
   describe 'GET /clients/:id' do
-    it 'returns the specified client' do
+    before :each do
       uuid = SecureRandom.uuid
-      client_id_url = "/clients/#{uuid}"
+      @client_id_url = "/clients/#{uuid}"
       FactoryGirl.create :client, id: uuid, name: 'Paint it All, Inc.'
+    end
 
-      get client_id_url,
-          headers: {
-            'Content-Type' => 'application/json',
-            'Authorization' => user.access_token
-          }
+    context 'with authorization' do
+      it 'returns the specified client' do
+        get @client_id_url,
+            headers: {
+              'Content-Type' => 'application/json',
+              'Authorization' => user.access_token
+            }
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+        expect(json['name']) == 'Paint it All, Inc.'
+      end
+    end
 
-      body = JSON.parse(response.body)
-      client_name = body['name']
-      expect(client_name) == 'Paint it All, Inc.'
+    context 'without authorization' do
+      before :each do
+        get @client_id_url,
+            headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
     end
   end
 
   describe 'POST /clients' do
-    it 'creates the specified client' do
-      client_request = attributes_for(:client)
-
-      post '/clients',
-           params: client_request.to_json,
-           headers: {
-             'Content-Type' => 'application/json',
-             'Authorization' => user.access_token
-           }
-
-      expect(response.status).to eq 201
-
-      body = JSON.parse(response.body)
-      client_name = body['name']
-      expect(client_name) == client_request['name']
+    before :each do
+      @client_request = attributes_for(:client)
     end
-  end
 
-  describe 'PUT /clients/:id' do
-    it 'updates the specified client' do
-      uuid = SecureRandom.uuid
-      client_id_url = "/clients/#{uuid}"
-      FactoryGirl.create :client, id: uuid, name: 'Paint it All, Inc.'
-
-      client_request = {
-        id: uuid.to_s,
-        title: 'Ship it'
-      }
-
-      put client_id_url,
-          params: client_request.to_json,
-          headers: {
-            'Content-Type' => 'application/json',
-            'Authorization' => user.access_token
-          }
-
-      expect(response.status).to eq 200
-
-      body = JSON.parse(response.body)
-      client_name = body['name']
-      expect(client_name) == client_request['name']
-    end
-  end
-
-  describe 'DELETE /clients/:id' do
-    it 'deletes the specified client' do
-      uuid = SecureRandom.uuid
-      client_id_url = "/clients/#{uuid}"
-      FactoryGirl.create :client, id: uuid
-
-      delete client_id_url,
+    context 'with authorization' do
+      it 'creates the specified client' do
+        post '/clients',
+             params: @client_request.to_json,
              headers: {
                'Content-Type' => 'application/json',
                'Authorization' => user.access_token
              }
 
-      expect(response.status).to eq 204
+        expect(response.status).to eq 201
+        expect(json['name']) == @client_request['name']
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        post '/clients',
+             params: @client_request.to_json,
+             headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
+    end
+  end
+
+  describe 'PUT /clients/:id' do
+    before :each do
+      uuid = SecureRandom.uuid
+      @client_id_url = "/clients/#{uuid}"
+      FactoryGirl.create :client, id: uuid, name: 'Paint it All, Inc.'
+
+      @client_request = {
+        id: uuid.to_s,
+        name: 'Ship it'
+      }
+    end
+
+    context 'with authorization' do
+      it 'updates the specified client' do
+        put @client_id_url,
+            params: @client_request.to_json,
+            headers: {
+              'Content-Type' => 'application/json',
+              'Authorization' => user.access_token
+            }
+
+        expect(response.status).to eq 200
+        expect(json['name']) == @client_request['name']
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        put @client_id_url,
+            params: @client_request.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
+    end
+  end
+
+  describe 'DELETE /clients/:id' do
+    before :each do
+      uuid = SecureRandom.uuid
+      @client_id_url = "/clients/#{uuid}"
+      FactoryGirl.create :client, id: uuid
+    end
+
+    context 'with authorization' do
+      it 'deletes the specified client' do
+        delete @client_id_url,
+               headers: {
+                 'Content-Type' => 'application/json',
+                 'Authorization' => user.access_token
+               }
+
+        expect(response.status).to eq 204
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        delete @client_id_url,
+               headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
     end
   end
 end
