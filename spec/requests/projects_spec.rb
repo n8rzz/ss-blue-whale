51edd2cf7162e6ab97edc6e26db1c71da062ded4
Nate Geslin <teamtomkins23@gmail.com>
2016-04-21 22:32:19 -0500

    Adds without authentication contexts to projects spec

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -4,73 +4,184 @@ describe 'Projects', :type => :request do
   let(:user) { create(:user) }
 
   describe 'GET /projects' do
-    it 'returns all projects' do
+    before :each do
       FactoryGirl.create_list(:project, 3)
+    end
 
-      get '/projects'
+    context 'with authorization' do
+      it 'returns all projects' do
+        get '/projects',
+            headers: {
+              'Content-Type' => 'application/json',
+              'Authorization' => user.access_token
+            }
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        get '/projects',
+            headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
     end
   end
 
   describe 'GET /projects/:id' do
-    it 'returns the specified project' do
+    before :each do
       FactoryGirl.create :project, id: 1
+    end
 
-      get '/projects/1'
+    context 'with authorization' do
+      it 'returns the specified project' do
+        get '/projects/1',
+            headers: {
+              'Content-Type' => 'application/json',
+              'Authorization' => user.access_token
+            }
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        get '/projects/1',
+            headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
     end
   end
 
   describe 'POST /projects' do
-    it 'creates the specified project' do
+    before :each do
       FactoryGirl.create(:client, id: 1)
       FactoryGirl.create(:project_type, id: 1)
-      project_request = attributes_for(:project, client_id: 1, project_type_id: 1)
 
-      post '/projects',
-           params: project_request.to_json,
-           headers: {
-             'Content-Type' => 'application/json',
-             'Authorization' => user.access_token
-           }
-
-      expect(response.status).to eq 201
+      @project_request = attributes_for(:project, client_id: 1, project_type_id: 1)
     end
-  end
 
-  describe 'PUT /projects/:id' do
-    it 'updates the specified project' do
-      FactoryGirl.create :project, id: 1
-
-      project_request = {
-        id: 1,
-        startDate: 2.days.ago
-      }
-
-      put '/projects/1',
-          params: project_request.to_json,
-          headers: {
-            'Content-Type' => 'application/json',
-            'Authorization' => user.access_token
-          }
-
-      expect(response.status).to eq 200
-    end
-  end
-
-  describe 'DELETE /projects/:id' do
-    it 'deletes the specified project' do
-      FactoryGirl.create :project, id: 1
-
-      delete '/projects/1',
+    context 'with authorization' do
+      it 'creates the specified project' do
+        post '/projects',
+             params: @project_request.to_json,
              headers: {
                'Content-Type' => 'application/json',
                'Authorization' => user.access_token
              }
 
-      expect(response.status).to eq 204
+        expect(response.status).to eq 201
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        post '/projects',
+             params: @project_request.to_json,
+             headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
+    end
+  end
+
+  describe 'PUT /projects/:id' do
+    before :each do
+      FactoryGirl.create :project, id: 1
+
+      @project_request = {
+        id: 1,
+        startDate: 2.days.ago
+      }
+    end
+
+    context 'with authorization' do
+      it 'updates the specified project' do
+        put '/projects/1',
+            params: @project_request.to_json,
+            headers: {
+              'Content-Type' => 'application/json',
+              'Authorization' => user.access_token
+            }
+
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        put '/projects/1',
+            params: @project_request.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
+    end
+  end
+
+  describe 'DELETE /projects/:id' do
+    before :each do
+      FactoryGirl.create :project, id: 1
+    end
+
+    context 'with authorization' do
+      it 'deletes the specified project' do
+        delete '/projects/1',
+               headers: {
+                 'Content-Type' => 'application/json',
+                 'Authorization' => user.access_token
+               }
+
+        expect(response.status).to eq 204
+      end
+    end
+
+    context 'without authorization' do
+      before :each do
+        delete '/projects/1',
+               headers: { 'Content-Type' => 'application/json' }
+      end
+
+      it 'request succeeds' do
+        expect(response.status).to eq 200
+      end
+
+      it 'response contains error metadata' do
+        expect(json['error']).to eq 'unauthorized'
+        expect(json['status']).to eq 401
+      end
     end
   end
 end
